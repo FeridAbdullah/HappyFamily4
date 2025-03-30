44add9811de5d7e7619d5e868797e3fce5c89292
FeridAbdullah <farid.z.guliyev.std@bhos.edu.az>
2025-03-30 11:10:42 +0400

    Human.java

---
+++ Human.java
@@ -0,0 +1,10 @@
+package az.edu.bhos.lesson11;
+
+public class Human {
+    String name;
+    String surname;
+
+    public void greetPet() {
+        System.out.println("Hello, lovely pet!");
+    }
+}
