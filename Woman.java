7947d9580067e8842cd0090a64cd635ba269e574
FeridAbdullah <farid.z.guliyev.std@bhos.edu.az>
2025-03-30 11:11:35 +0400

    Woman.java

---
+++ Woman.java
@@ -0,0 +1,11 @@
+package az.edu.bhos.lesson11;
+
+public class Woman extends Human {
+    public void greetPet() {
+        System.out.println("Hello, lovely pet!");
+    }
+
+    public void makeup() {
+        System.out.println("I am doing makeup.");
+    }
+}
