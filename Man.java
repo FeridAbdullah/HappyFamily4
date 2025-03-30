cfe6418040e332b86fb08f0d052296dc43a59f9d
FeridAbdullah <farid.z.guliyev.std@bhos.edu.az>
2025-03-30 11:11:08 +0400

    Man.java

---
+++ Man.java
@@ -0,0 +1,11 @@
+package az.edu.bhos.lesson11;
+
+public class Man extends Human {
+    public void greetPet() {
+        System.out.println("Hey, buddy!");
+    }
+
+    public void repairCar() {
+        System.out.println("I am repairing the car.");
+    }
+}
